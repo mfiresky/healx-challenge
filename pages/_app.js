0bd0f0a7821ae107c9092e34a1c17a2b3a902706
Bartosz Fajerski <bartosz.fajerski@gmail.com>
2021-07-19 02:03:18 +0100

    chore: add full search page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import {ApolloProvider} from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+function MyApp({Component, pageProps}) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:3000/api/graphql',
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
